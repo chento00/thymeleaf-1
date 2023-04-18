acf0b117b525edcb610fc90fa17b75d36d9b6d82
chento007 <chentochea2002@gmail.com>
2023-04-18 13:51:56 +0700

    spring-thymeleaf-1

---
+++ src/main/java/co/devops/thymeleaf3/controller/ArticleController.java
@@ -0,0 +1,12 @@
+package co.devops.thymeleaf3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ArticleController {
+    @GetMapping("article")
+    String viewArticle(){
+        return "pages/article";
+    }
+}
